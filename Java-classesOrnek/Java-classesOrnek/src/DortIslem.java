5c03b019ab9d47cc82e585bfed64a47011fb541f
MertKx <113070286+MertKx@users.noreply.github.com>
2023-06-11 19:36:43 +0300

    Add files via upload

---
+++ Java-classesOrnek/Java-classesOrnek/src/DortIslem.java
@@ -0,0 +1,15 @@
+public class DortIslem {
+    public int topla(int say1,int say2) {
+        return say1+say2;
+    }
+    public int cikar(int say1,int say2) {
+        return say1 - say2;
+    }
+
+    public int carp(int say1,int say2) {
+        return say1*say2;
+    }
+    public int bol(int say1,int say2) {
+        return say1/say2;
+    }
+}
---
+++ Java-classesOrnek/Java-classesOrnek/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        System.out.println(dortIslem.cikar(9,3));
+        System.out.println(dortIslem.bol(12,3));
+        System.out.println(dortIslem.carp(4,8));
+        System.out.println(dortIslem.topla(4,7));
+    }
+}
